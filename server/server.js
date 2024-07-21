a63e62719a8e4a60afbfd6996a6858c4a6f6d8e9
Lungeli <kawai.magar7@gmail.com>
2024-07-22 00:29:11 +0545

    Auth Routes Setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 const app = express();
-
+import authRoutes from "./routes/auth.routes.js";
 dotenv.config()
 
 const PORT = process.env.PORT
@@ -9,5 +9,8 @@ const PORT = process.env.PORT
 app.get("/", (req, res) => {
     res.send("Server is ready Wamma Jamma");
 })
+
+
+app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => console.log(`Server is running on Port ${PORT}`));
