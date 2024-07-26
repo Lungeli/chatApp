87544793865ba32fe45e015de9061fcaecc64380
Lungeli <kawai.magar7@gmail.com>
2024-07-26 15:20:13 +0545

    Generate JWT, Login and Logout Endpoint

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
    
@@ -44,7 +45,7 @@ export const signup = async (req, res) => {
 
         if(newUser){
             await newUser.save();
-        
+            generateTokenAndSetCookie(newUser._id, res);
             res.status(201).json({
                 _id: newUser._id,
                 fullName: newUser.fullName,
@@ -61,10 +62,39 @@ export const signup = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    console.log("Login User");
+export const login = async (req, res) => {
+    try {
+        const {userName, password} = req.body;
+        const user = await User.findOne({userName})
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error: "Invalid Creds"});
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            userName: user.userName,
+            profilePic: user.profilePic,
+
+        });
+        
+    } catch (error) {
+        console.log("Error in Login Controller", error.message);
+        res.status(500).json({error:"Internal Server Error"});
+        
+    }
 }
 
 export const logout = (req, res) => {
-    console.log("Logout User");
+    try {
+        res.cookie("jwt", "", {maxAge: 0});
+        res.status(200).json({message: "Logged Out Sucessfully"});
+    } catch (error) {
+        console.log("Error in Logout Controller", error.message);
+        res.status(500).json({error:"Internal Server Error"});  
+    }
 }
