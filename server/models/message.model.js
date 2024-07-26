a59d8c8021a37effbdb6ebf9286548a42ceffa89
Lungeli <kawai.magar7@gmail.com>
2024-07-26 16:37:23 +0545

    Message and Conversation model

---
+++ server/models/message.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    senderID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: TextTrackCue,
+    },
+    recieverID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: TextTrackCue,
+    },
+    message: {
+        type: String,
+        required: true
+    }
+    //createdAt, updatedAt
+}, {timestamps: true});
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
