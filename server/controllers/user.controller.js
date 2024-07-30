4419af89e05d7dfafedd8a01a23c7efd582641f7
Lungeli <kawai.magar7@gmail.com>
2024-07-31 01:12:50 +0545

    Client side Login page, Sinup page and home page

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -8,6 +8,8 @@ export const getUsersForSideBar = async( req, res) => {
 
         res.status(200).json(filteredUsers);
 
+        
+
     } catch (error) {
         console.log("Error in getUserSideBar: ", error.message);
         res.status(500).json({error: "Internal Server Error"});
